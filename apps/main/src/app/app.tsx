00cd2f87a45a738077a97dd41029bc49ffa2ea48
Andrey Mashukov <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>
2024-06-24 21:25:37 +0800

    changes

--- apps/main/src/app/app.tsx
+++ apps/main/src/app/app.tsx
@@ -4,7 +4,7 @@ axios.defaults.baseURL = process.env.NX_REACT_APP_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
-  return <>Main</>;
+  return <>Main App</>;
 }
 
 export default App;
